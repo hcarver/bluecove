ba492578d9794e4161c068fc85c42646cb84f83f
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2007-02-14 21:13:34 +0000

    Updated headers
    git-svn-id: http://bluecove.googlecode.com/svn/trunk@60 3cac39a0-5d1b-0410-8711-c1891c4e0f47

--- src/com/sun/cdc/io/j2me/btspp/Protocol.java
+++ src/com/sun/cdc/io/j2me/btspp/Protocol.java
@@ -1,3 +1,22 @@
+/**
+ *  BlueCove - Java library for Bluetooth
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  @version $Id: Connection.java 877 2007-02-13 02:24:09Z vlads $
+ */
 package com.sun.cdc.io.j2me.btspp;
 
 import java.io.IOException;
@@ -9,8 +28,7 @@ import com.sun.cdc.io.ConnectionBaseInterface;
 
 public class Protocol implements ConnectionBaseInterface {
 
-	public Connection openPrim(String name, int mode, boolean timeouts)
-			throws IOException {
+	public Connection openPrim(String name, int mode, boolean timeouts) throws IOException {
 		return MicroeditionConnector.open("btspp:" + name, mode, timeouts);
 	}
 
