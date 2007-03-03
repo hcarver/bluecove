ef15f599c8cd1f6e07c9b1ae78d8ed72f87bf4ce
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2007-03-03 00:26:16 +0000

    Started bluecove-tester
    git-svn-id: http://bluecove.googlecode.com/svn/trunk@128 3cac39a0-5d1b-0410-8711-c1891c4e0f47

--- test/com/intel/bluetooth/test/Consts.java
+++ test/com/intel/bluetooth/test/Consts.java
@@ -19,9 +19,10 @@
  */
 package com.intel.bluetooth.test;
 
-public class Consts {
+public interface Consts {
 
 	public static final String TEST_UUID = "27012f0c68af4fbf8dbe6bbaf7ab651b";
 
 	public static final String TEST_SERVERNAME_PREFIX = "bluecoveSrv";
+	
 }
