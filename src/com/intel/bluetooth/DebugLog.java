512bab3e919383dae9f6015080c4e419e4e3ad78
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2007-04-30 18:13:47 +0000

    Allow to inject LoggerAppender
    git-svn-id: http://bluecove.googlecode.com/svn/trunk@192 3cac39a0-5d1b-0410-8711-c1891c4e0f47

--- src/com/intel/bluetooth/DebugLog.java
+++ src/com/intel/bluetooth/DebugLog.java
@@ -20,8 +20,10 @@
  */
 package com.intel.bluetooth;
 
+import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Vector;
 
 /**
  * The methods of this calls would be removed automaticaly because they are
@@ -33,6 +35,10 @@ import java.util.Set;
 public class DebugLog {
 
 	private static final boolean debugCompiledOut = false;
+
+	public final static int DEBUG = 1;
+
+	public final static int ERROR = 4;
 	
 	private static boolean debugEnabled = false;
 
@@ -43,16 +49,23 @@ public class DebugLog {
 	private static final Set fqcnSet = new HashSet();
 	
 	private static boolean java13 = false;
+
+	private static Vector loggerAppenders = new Vector();
 	
-	 static {
-	      fqcnSet.add(FQCN);
-	 }
+	public static interface LoggerAppender {
+		public void appendLog(int level, String message, Throwable throwable);
+	}
+
+	static {
+		fqcnSet.add(FQCN);
+	}
 	
 	private static void initialize() {
 		initialized = true;
 		String d = System.getProperty("bluecove.debug");
 		debugEnabled = ((d != null) && (d.equalsIgnoreCase("true") || d.equalsIgnoreCase("1")));
 		if (debugEnabled && debugCompiledOut) {
+			debugEnabled = false;
 			System.err.println("BlueCove debug functions have been Compiled Out");
 		}
 	}
@@ -68,6 +81,7 @@ public class DebugLog {
 		if (!debugCompiledOut && isDebugEnabled()) {
 			System.out.println(message);
 			printLocation();
+			callAppenders(DEBUG, message, null);
 		}
 	}
 	
@@ -75,6 +89,7 @@ public class DebugLog {
 		if (!debugCompiledOut && isDebugEnabled()) {
 			System.out.println(message + " " + v);
 			printLocation();
+			callAppenders(DEBUG, message + " " + v, null);
 		}
 	}
 
@@ -82,6 +97,7 @@ public class DebugLog {
 		if (!debugCompiledOut && isDebugEnabled()) {
 			System.out.println(message + " " + obj.toString());
 			printLocation();
+			callAppenders(DEBUG, message + " " + obj.toString(), null);
 		}
 	}
 	
@@ -89,6 +105,7 @@ public class DebugLog {
 		if (!debugCompiledOut && isDebugEnabled()) {
 			System.out.println(message + " " + v + " " + v2);
 			printLocation();
+			callAppenders(DEBUG, message + " " + v + " " + v2, null);
 		}
 	}
 	
@@ -96,6 +113,7 @@ public class DebugLog {
 		if (!debugCompiledOut && isDebugEnabled()) {
 			System.out.println(message + " " + String.valueOf(v));
 			printLocation();
+			callAppenders(DEBUG, message + " " + String.valueOf(v), null);
 		}
 	}
 
@@ -103,6 +121,15 @@ public class DebugLog {
 		if (!debugCompiledOut && isDebugEnabled()) {
 			System.out.println(message + " " + v);
 			printLocation();
+			callAppenders(DEBUG, message + " " + v, null);
+		}
+	}
+	
+	public static void error(String message) {
+		if (!debugCompiledOut && isDebugEnabled()) {
+			System.out.println("error " + message);
+			printLocation();
+			callAppenders(ERROR, message, null);
 		}
 	}
 	
@@ -110,6 +137,7 @@ public class DebugLog {
 		if (!debugCompiledOut && isDebugEnabled()) {
 			System.out.println("error " + message + " " + v);
 			printLocation();
+			callAppenders(ERROR, message + " " + v, null);
 		}
 	}
 	
@@ -117,6 +145,7 @@ public class DebugLog {
 		if (!debugCompiledOut && isDebugEnabled()) {
 			System.out.println("error " + message + " " + v);
 			printLocation();
+			callAppenders(ERROR, message + " " + v, null);
 		}
 	}
 	
@@ -125,6 +154,7 @@ public class DebugLog {
 			System.out.println("error " + message + " " + t);
 			printLocation();
 			t.printStackTrace(System.out);
+			callAppenders(ERROR, message, t);
 		}
 	}
 
@@ -132,6 +162,22 @@ public class DebugLog {
 		System.out.println("error " + message + " " + t);
 		printLocation();
 		t.printStackTrace(System.out);
+		callAppenders(ERROR, message, t);
+	}
+	
+	private static void callAppenders(int level, String message, Throwable throwable) {
+		for (Enumeration iter = loggerAppenders.elements(); iter.hasMoreElements();) {
+			LoggerAppender a = (LoggerAppender) iter.nextElement();
+			a.appendLog(level, message, throwable);
+		};
+	}
+	
+	public static void addAppender(LoggerAppender newAppender) {
+		loggerAppenders.addElement(newAppender);
+	}
+	
+	public static void removeAppender(LoggerAppender newAppender) {
+		loggerAppenders.removeElement(newAppender);
 	}
 	
 	private static void printLocation() {
