0e171fefd2a76458f97519af4349e8b782d2b3ca
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2007-07-10 05:50:06 +0000

    add comments
    git-svn-id: http://bluecove.googlecode.com/svn/trunk@669 3cac39a0-5d1b-0410-8711-c1891c4e0f47

--- src/main/java/com/ibm/oti/vm/VM.java
+++ src/main/java/com/ibm/oti/vm/VM.java
@@ -2,7 +2,7 @@ package com.ibm.oti.vm;
 
 import java.io.IOException;
 
-/*
+/**
  * Licensed Materials - Property of IBM,
  * (c) Copyright IBM Corp. 2000, 2002  All Rights Reserved
  * 
@@ -11,7 +11,14 @@ import java.io.IOException;
 
 public class VM {
 
-	 public static synchronized void loadLibrary(String libName) throws IOException {
+	/**
+	 * Stub for IBM J9 implemenation.
+	 * Loads the system library specified by the libname argument. 
+	 * 
+	 * @param libname
+	 * @throws IOException
+	 */
+	 public static synchronized void loadLibrary(String libname) throws IOException {
 		 throw new IOException("Should not be used");
 	 }
 
