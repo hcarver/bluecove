be2a2da0352595204a6715acbe603cbd96915122
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2007-08-23 05:48:20 +0000

    work on Quick Tutorial
    git-svn-id: http://bluecove.googlecode.com/svn/trunk@970 3cac39a0-5d1b-0410-8711-c1891c4e0f47

---
+++ src/test/java/com/intel/bluetooth/javadoc/RemoteDeviceDiscovery.java
@@ -0,0 +1,74 @@
+/**
+ *  BlueCove - Java library for Bluetooth
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  @version $Id$
+ */
+package com.intel.bluetooth.javadoc;
+
+import java.io.IOException;
+import java.util.Vector;
+
+import javax.bluetooth.*;
+
+/**
+ * @author vlads
+ *
+ * Minimal DeviceDiscovery example for javadoc.
+ */
+public class RemoteDeviceDiscovery {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+
+		final Object inquiryCompletedEvent = new Object();
+		
+		final Vector devicesDiscovered = new Vector();
+		
+		DiscoveryListener listener = new DiscoveryListener() {
+
+			public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
+				System.out.println("Device " + btDevice.getBluetoothAddress() + " found");
+				devicesDiscovered.addElement(btDevice);
+				try {
+					System.out.println("     name " + btDevice.getFriendlyName(false));
+				} catch (IOException cantGetDeviceName) {
+				}
+			}
+
+			public void inquiryCompleted(int discType) {
+				System.out.println("Device Inquiry completed!");
+				synchronized(inquiryCompletedEvent){
+					inquiryCompletedEvent.notifyAll();
+		        }
+			}
+
+			public void serviceSearchCompleted(int transID, int respCode) {
+			}
+
+			public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
+			}
+		};
+		
+		synchronized(inquiryCompletedEvent) {
+			boolean started = LocalDevice.getLocalDevice().getDiscoveryAgent().startInquiry(DiscoveryAgent.GIAC, listener);
+			if (started) {
+				System.out.println("wait for device inquiry to complete...");
+				inquiryCompletedEvent.wait();
+				System.out.println(devicesDiscovered.size() +  " device(s) found");
+			}
+		}
+	}
+}
---
+++ src/test/java/com/intel/bluetooth/javadoc/ServicesSearch.java
@@ -0,0 +1,39 @@
+/**
+ *  BlueCove - Java library for Bluetooth
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  @version $Id$
+ */
+package com.intel.bluetooth.javadoc;
+
+import java.io.IOException;
+
+/**
+ * @author vlads
+ *
+ * Minimal ServicesSearch example for javadoc.
+ */
+public class ServicesSearch {
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
+		// First run RemoteDeviceDiscovery and use discoved device
+		RemoteDeviceDiscovery.main(null);
+	
+		//TODO
+	}
+
+}
