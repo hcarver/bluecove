9d2f4012962682b300e20446cff817c3757806e4
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2007-10-13 02:29:39 +0000

    test cldc api 1.0/1.1 javax.microedition.io signature
    git-svn-id: http://bluecove.googlecode.com/svn/trunk@1074 3cac39a0-5d1b-0410-8711-c1891c4e0f47

---
+++ src/test/java/javax/microedition/io/CLDC11APIDeclarationsTest.java
@@ -0,0 +1,40 @@
+/**
+ *  BlueCove - Java library for Bluetooth
+ *  Copyright (C) 2007 Vlad Skarzhevskyy
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  @version $Id$
+ */
+package javax.microedition.io;
+
+import net.sf.jour.signature.SignatureTestCase;
+
+/**
+ * @author vlads
+ *
+ */
+public class CLDC11APIDeclarationsTest extends SignatureTestCase {
+
+    public String getAPIPath() {
+        return getClassPath(Connection.class);
+    }
+
+    public String getSignatureXMLPath() {
+        return "cldcapi_10_11-javax.microedition.io-signature.xml";
+    }
+
+
+}
