52c5c74161560bc5082a1b7fe357ef770beb7687
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2007-11-20 21:57:47 +0000

    updated types
    git-svn-id: http://bluecove.googlecode.com/svn/trunk@1222 3cac39a0-5d1b-0410-8711-c1891c4e0f47

--- src/main/c/intelbth/OSXStackSDPQuery.mm
+++ src/main/c/intelbth/OSXStackSDPQuery.mm
@@ -16,7 +16,7 @@
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- *  @version $Id: OSXStackDiscovery.mm 1174 2007-11-12 04:19:25Z skarzhevskyy $
+ *  @version $Id$
  */
 
 #import "OSXStackSDPQuery.h"
