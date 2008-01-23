f4e3a01f8039df92a36b9cb2832327665919f136
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2008-01-23 00:34:13 +0000

    Sort service attributes by ID in SDP
    git-svn-id: http://bluecove.googlecode.com/svn/trunk@1662 3cac39a0-5d1b-0410-8711-c1891c4e0f47

--- src/main/java/com/intel/bluetooth/ServiceRecordImpl.java
+++ src/main/java/com/intel/bluetooth/ServiceRecordImpl.java
@@ -66,11 +66,36 @@ class ServiceRecordImpl implements ServiceRecord {
 	byte[] toByteArray() throws IOException {
 		DataElement element = new DataElement(DataElement.DATSEQ);
 
-		for (Enumeration e = attributes.keys(); e.hasMoreElements();) {
-			Integer key = (Integer) e.nextElement();
+		final boolean sort = true;
+		if (sort) {
+			int[] sortIDs = new int[attributes.size()];
+			int k = 0;
+			for (Enumeration e = attributes.keys(); e.hasMoreElements();) {
+				Integer key = (Integer) e.nextElement();
+				sortIDs[k] = key.intValue();
+				k++;
+			}
+			// Sort
+			for (int i = 0; i < sortIDs.length; i++) {
+				for (int j = 0; j < sortIDs.length - i - 1; j++) {
+					if (sortIDs[j] > sortIDs[j + 1]) {
+						int temp = sortIDs[j];
+						sortIDs[j] = sortIDs[j + 1];
+						sortIDs[j + 1] = temp;
+					}
+				}
+			}
+			for (int i = 0; i < sortIDs.length - 1; i++) {
+				element.addElement(new DataElement(DataElement.U_INT_2, sortIDs[i]));
+				element.addElement(getAttributeValue(sortIDs[i]));
+			}
+		} else {
+			for (Enumeration e = attributes.keys(); e.hasMoreElements();) {
+				Integer key = (Integer) e.nextElement();
 
-			element.addElement(new DataElement(DataElement.U_INT_2, key.intValue()));
-			element.addElement((DataElement) attributes.get(key));
+				element.addElement(new DataElement(DataElement.U_INT_2, key.intValue()));
+				element.addElement((DataElement) attributes.get(key));
+			}
 		}
 
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
