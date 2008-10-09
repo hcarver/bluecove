5c7bb55698377d3f9cac545247b20543b0b295bc
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2008-10-09 17:51:47 +0000

    remove invalid tests since it depends on order in HashMap
    git-svn-id: http://bluecove.googlecode.com/svn/trunk@2415 3cac39a0-5d1b-0410-8711-c1891c4e0f47

--- bluecove-emu-gui/src/test/java/com/bluecove/emu/gui/model/ModelUpdateTest.java
+++ bluecove-emu-gui/src/test/java/com/bluecove/emu/gui/model/ModelUpdateTest.java
@@ -17,7 +17,7 @@ import com.intel.bluetooth.emu.MonitorDevice;
 public class ModelUpdateTest extends TestCase implements Observer {
 
 	private Model model;
-	
+
 	protected void setUp() throws Exception {
 		super.setUp();
 		try {
@@ -33,11 +33,10 @@ public class ModelUpdateTest extends TestCase implements Observer {
 		newDevices1.add(new MonitorDeviceMock(23, "23-1", 1));
 		newDevices1.add(new MonitorDeviceMock(24, "24-1", 1));
 		newDevices1.add(new MonitorDeviceMock(25, "25-1", 1));
-		
-		
+
 		model.updateDeviceList(newDevices1);
 		assertEquals(4, newDevices1.size());
-		
+
 		List<MonitorDevice> newDevices2 = new ArrayList<MonitorDevice>();
 		newDevices2.add(new MonitorDeviceMock(32, "32-2", 1));
 		newDevices2.add(new MonitorDeviceMock(23, "23-2", 1));
@@ -45,17 +44,20 @@ public class ModelUpdateTest extends TestCase implements Observer {
 		newDevices2.add(new MonitorDeviceMock(35, "35-2", 1));
 		model.updateDeviceList(newDevices2);
 		assertEquals(4, newDevices2.size());
-		assertEquals("23-2", model.devices.get(0).getMonitorDevice().getDeviceDescriptor().getName());
-		assertEquals("24-2", model.devices.get(1).getMonitorDevice().getDeviceDescriptor().getName());
-		assertEquals("32-2", model.devices.get(2).getMonitorDevice().getDeviceDescriptor().getName());
-		assertEquals("35-2", model.devices.get(3).getMonitorDevice().getDeviceDescriptor().getName());
+		// This is invalid tests since it depends on order in HashMap
+		// assertEquals("23-2",
+		// model.devices.get(0).getMonitorDevice().getDeviceDescriptor().getName());
+		// assertEquals("24-2",
+		// model.devices.get(1).getMonitorDevice().getDeviceDescriptor().getName());
+		// assertEquals("32-2",
+		// model.devices.get(2).getMonitorDevice().getDeviceDescriptor().getName());
+		// assertEquals("35-2",
+		// model.devices.get(3).getMonitorDevice().getDeviceDescriptor().getName());
 
-		
 	}
 
 	public void update(Observable o, Object arg) {
-		System.out.println(arg);		
+		System.out.println(arg);
 	}
-
 
 }
