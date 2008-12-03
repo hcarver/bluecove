ec3df684a958c84895d72bc291edc952213a7dcd
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2008-12-03 19:12:28 +0000

    OS X build will fail in case of error
    git-svn-id: http://bluecove.googlecode.com/svn/trunk@2487 3cac39a0-5d1b-0410-8711-c1891c4e0f47

--- bluecove/build-native-osx.sh
+++ bluecove/build-native-osx.sh
@@ -7,7 +7,12 @@ BUILD_ROOT=`pwd`
 cd src/main/c/intelbth
 
 xcodebuild
+BUILD_ERROR_CODE=$?
 
 cd ${BUILD_ROOT}
 
 cp src/main/resources/libbluecove.jnilib target/classes/
+
+if [[ ${BUILD_ERROR_CODE} != 0  ]] ; then
+  exit ${BUILD_ERROR_CODE}
+fi
