a7a46008d8ea690d2e4501abf02e3e1eea725c9b
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2008-12-09 20:23:14 +0000

    sync package javax.obex javadocs with JSR-82 1.1.1
    git-svn-id: http://bluecove.googlecode.com/svn/trunk@2532 3cac39a0-5d1b-0410-8711-c1891c4e0f47

--- bluecove/src/main/java/javax/obex/PasswordAuthentication.java
+++ bluecove/src/main/java/javax/obex/PasswordAuthentication.java
@@ -31,51 +31,50 @@ import com.intel.bluetooth.Utils;
 /**
  * This class holds user name and password combinations.
  * 
- * @version 1.0 February 11, 2002
  */
 public class PasswordAuthentication {
 
-	private byte[] userName;
-	
-	private byte[] password;
-	
-	/**
-	 * Creates a new <code>PasswordAuthentication</code> with the user name
-	 * and password provided.
-	 * 
-	 * @param userName
-	 *            the user name to include; this may be <code>null</code>
-	 * 
-	 * @param password
-	 *            the password to include in the response
-	 * 
-	 * @exception NullPointerException
-	 *                if <code>password</code> is <code>null</code>
-	 */
-	public PasswordAuthentication(byte[] userName, byte[] password) {
-		if (password == null) {
-			throw new NullPointerException("password is null");
-		}
-		this.userName = Utils.clone(userName);
-		this.password = Utils.clone(password);
-	}
+    private byte[] userName;
 
-	/**
-	 * Retrieves the user name that was specified in the constructor. The user
-	 * name may be <code>null</code>.
-	 * 
-	 * @return the user name
-	 */
-	public byte[] getUserName() {
-		return Utils.clone(this.userName);
-	}
+    private byte[] password;
 
-	/**
-	 * Retrieves the password.
-	 * 
-	 * @return the password
-	 */
-	public byte[] getPassword() {
-		return Utils.clone(this.password);
-	}
+    /**
+     * Creates a new <code>PasswordAuthentication</code> with the user name and
+     * password provided.
+     * 
+     * @param userName
+     *            the user name to include; this may be <code>null</code>
+     * 
+     * @param password
+     *            the password to include in the response
+     * 
+     * @exception NullPointerException
+     *                if <code>password</code> is <code>null</code>
+     */
+    public PasswordAuthentication(byte[] userName, byte[] password) {
+        if (password == null) {
+            throw new NullPointerException("password is null");
+        }
+        this.userName = Utils.clone(userName);
+        this.password = Utils.clone(password);
+    }
+
+    /**
+     * Retrieves the user name that was specified in the constructor. The user
+     * name may be <code>null</code>.
+     * 
+     * @return the user name
+     */
+    public byte[] getUserName() {
+        return Utils.clone(this.userName);
+    }
+
+    /**
+     * Retrieves the password.
+     * 
+     * @return the password
+     */
+    public byte[] getPassword() {
+        return Utils.clone(this.password);
+    }
 }
