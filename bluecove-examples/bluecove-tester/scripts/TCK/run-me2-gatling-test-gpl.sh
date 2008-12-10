f062094386c6f761c08e0f537e8f0871681e39a5
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2008-12-10 05:43:18 +0000

    TCK tests on Linux
    git-svn-id: http://bluecove.googlecode.com/svn/trunk@2540 3cac39a0-5d1b-0410-8711-c1891c4e0f47

--- bluecove-examples/bluecove-tester/scripts/TCK/run-me2-gatling-test-gpl.sh
+++ bluecove-examples/bluecove-tester/scripts/TCK/run-me2-gatling-test-gpl.sh
@@ -3,6 +3,11 @@
 #
 . ./tck-environment.sh
 
+if [[ ! -d ${MICROEMULATOR_HOME} ]] ; then
+  echo "Invalid Microemulator directory ${MICROEMULATOR_HOME}"
+  exit 1
+fi
+
 BLUECOVE_TCK_CP="${MICROEMULATOR_HOME}/microemulator.jar"
 BLUECOVE_TCK_CP="${BLUECOVE_TCK_CP}:${BLUECOVE_JAR}:${BLUECOVE_GPL_JAR}"
 
--- bluecove-examples/bluecove-tester/scripts/TCK/tck-environment.sh
+++ bluecove-examples/bluecove-tester/scripts/TCK/tck-environment.sh
@@ -2,10 +2,16 @@
 # @version $Revision$ ($Author$) $Date$
 #
 SCRIPTS_DIR=`dirname ${0}`/..
-. ${SCRIPTS_DIR}/../environment.sh
+. ${SCRIPTS_DIR}/environment.sh
 
 BLUECOVE_TCK_HOST=tckhost
 BLUECOVE_TCK_PORT=8080
 
+if [[ ! -d ${BLUECOVE_3RDPARTY_HOME} ]] ; then
+  echo "Invalid 3-rd party directory ${BLUECOVE_3RDPARTY_HOME}"
+  exit 1
+fi
+
 MICROEMULATOR_HOME=${BLUECOVE_3RDPARTY_HOME}/microemulator
+
 
