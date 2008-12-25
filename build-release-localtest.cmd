c9fb6870291503f8a01be2cd67d0949437d75ed6
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2008-12-25 20:29:23 +0000

    correction to local tests
    git-svn-id: http://bluecove.googlecode.com/svn/trunk@2668 3cac39a0-5d1b-0410-8711-c1891c4e0f47

--- build-release-localtest.cmd
+++ build-release-localtest.cmd
@@ -4,8 +4,8 @@ rem helper to run maven2
 rem
 if not defined MAVEN_OPTS  set MAVEN_OPTS=-Xmx256M
 
-call mvn clean
-@if errorlevel 1 goto errormark
+rem call mvn clean
+rem @if errorlevel 1 goto errormark
 
 call mvn clean deploy -P build -P release -DperformRelease=true
 @if errorlevel 1 goto errormark
