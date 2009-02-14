b460f40bc5ec6edda62f7ca202106294b05d63ad
skarzhevskyy <skarzhevskyy@3cac39a0-5d1b-0410-8711-c1891c4e0f47>
2009-02-14 02:05:10 +0000

    32 and 64 bit starters
    git-svn-id: http://bluecove.googlecode.com/svn/trunk@2751 3cac39a0-5d1b-0410-8711-c1891c4e0f47

---
+++ bluecove-examples/bluecove-tester/scripts/linux/64bit.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# @version $Revision$ ($Author$) $Date$
+#
+SCRIPTS_DIR=`dirname "${0}"`/..
+. "${SCRIPTS_DIR}/environment.sh"
+if [[ ! "$?" = "0" ]]; then
+    echo Error calling environment.sh
+    exit 1
+fi
+
+java -d64 -cp "${BLUECOVE_TESTER_CP}" ${BLUECOVE_MAIN} $*
+rc=$?
+if [[ ! "${rc}" = "0" ]]; then
+    echo Error calling java
+    echo Prsess enter to exit
+    read
+fi
